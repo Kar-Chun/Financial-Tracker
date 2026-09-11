d99bd43f7910a05851f88fc09f974a8b0769dd3b
Chang Kar Chun <241597Y@mymail.nyp.edu.sg>
2026-09-11 22:58:40 +0800

    fix: deployment failure

--- src/types/database.generated.ts
+++ src/types/database.generated.ts
@@ -1,6 +1,6 @@
-﻿Need to install the following packages:
-supabase@2.117.0
-Ok to proceed? (y) 
+﻿// Need to install the following packages:
+// supabase@2.117.0
+// Ok to proceed? (y) 
 export type Json =
   | string
   | number
